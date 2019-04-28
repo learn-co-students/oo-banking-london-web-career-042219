3fc86ea503ab743845f696584ae3d5a7806f9f16
github username <github email address>
2019-04-28 16:26:00 +0100

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
+require 'pry'
+require_relative './transfer.rb'
+
+
 class BankAccount
 
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name, balance=1000, status="open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
+  def deposit(amount)
+    @balance = @balance + (amount)
+  end
+
+  def display_balance
+   "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+
+
 end
+# binding.pry
+"lalal"
